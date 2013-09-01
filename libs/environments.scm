9fca8ea72ec693166959ec97fedc555db539559e
Yukang <moorekang@gmail.com>
2013-09-01 22:51:05 +0800

    continue ch3

---
+++ libs/environments.scm
@@ -0,0 +1,81 @@
+
+  ;; builds environment interface, using data structures defined in
+  ;; data-structures.scm.
+
+;;;;;;;;;;;;;;;; initial environment ;;;;;;;;;;;;;;;;
+
+  ;; init-env : () -> Env
+  ;; usage: (init-env) = [i=1, v=5, x=10]
+  ;; (init-env) builds an environment in which i is bound to the
+  ;; expressed value 1, v is bound to the expressed value 5, and x is
+  ;; bound to the expressed value 10.
+  ;; Page: 69
+
+
+;; environment datastructures
+
+(define empty-env-record
+  (lambda ()
+    '()))
+
+(define extended-env-record
+  (lambda (sym val old-env)
+    (cons (list sym val) old-env)))
+
+(define empty-env-record? null?)
+
+(define environment?
+  (lambda (x)
+    (or (empty-env-record? x)
+        (and (pair? x)
+             (symbol? (car (car x)))
+             (expval? (cadr (car x)))
+             (environment? (cdr x))))))
+
+(define extended-env-record->sym
+  (lambda (r)
+    (car (car r))))
+
+(define extended-env-record->val
+  (lambda (r)
+    (cadr (car r))))
+
+(define extended-env-record->old-env
+  (lambda (r)
+    (cdr r)))
+
+
+  (define init-env
+    (lambda ()
+      (extend-env
+       'i (num-val 1)
+       (extend-env
+        'v (num-val 5)
+        (extend-env
+         'x (num-val 10)
+         (empty-env))))))
+
+;;;;;;;;;;;;;;;; environment constructors and observers ;;;;;;;;;;;;;;;;
+
+  (define empty-env
+    (lambda ()
+      (empty-env-record)))
+
+  (define empty-env?
+    (lambda (x)
+      (empty-env-record? x)))
+
+  (define extend-env
+    (lambda (sym val old-env)
+      (extended-env-record sym val old-env)))
+
+  (define apply-env
+    (lambda (env search-sym)
+      (if (empty-env? env)
+	(eopl:error 'apply-env "No binding for ~s" search-sym)
+	(let ((sym (extended-env-record->sym env))
+	      (val (extended-env-record->val env))
+	      (old-env (extended-env-record->old-env env)))
+	  (if (eqv? search-sym sym)
+	    val
+	    (apply-env old-env search-sym))))))
