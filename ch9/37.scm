0b75aacd9cbbdf7c241c588de53f3aa9108cca20
Yukang <moorekang@gmail.com>
2014-03-23 14:37:25 +0800

    continue 9.37

--- ch9/37.scm
+++ ch9/37.scm
@@ -12,4 +12,165 @@
 (load-relative "./base/typed-oo/tests.scm")
 
 
-(run-all)
+;;;;;;;;;;;;;;;; grammatical specification ;;;;;;;;;;;;;;;;
+(define the-lexical-spec
+  '((whitespace (whitespace) skip)
+    (comment ("%" (arbno (not #\newline))) skip)
+    (identifier
+     (letter (arbno (or letter digit "_" "-" "?")))
+     symbol)
+    (number (digit (arbno digit)) number)
+    (number ("-" digit (arbno digit)) number)
+    ))
+
+(define the-grammar
+  '((program ((arbno class-decl) expression) a-program)
+
+    (expression (number) const-exp)
+    (expression
+     ("-" "(" expression "," expression ")")
+     diff-exp)
+
+    (expression
+     ("+" "(" expression "," expression ")")
+     sum-exp)
+
+    (expression
+     ("zero?" "(" expression ")")
+     zero?-exp)
+
+    (expression
+     ("if" expression "then" expression "else" expression)
+     if-exp)
+
+    (expression (identifier) var-exp)
+
+    (expression
+     ("let" (arbno identifier "=" expression) "in" expression)
+     let-exp)
+
+    (expression
+     ("proc" "(" (separated-list identifier ":" type ",") ")" expression)
+     proc-exp)
+
+    (expression
+     ("(" expression (arbno expression) ")")
+     call-exp)
+
+    (expression
+     ("letrec"
+      (arbno type identifier "(" (separated-list identifier ":" type ",") ")"
+             "=" expression)
+      "in" expression)
+     letrec-exp)
+
+    (expression
+     ("begin" expression (arbno ";" expression) "end")
+     begin-exp)
+
+    (expression
+     ("set" identifier "=" expression)
+     assign-exp)
+
+    ;; non-empty lists for typechecked version
+    (expression
+     ("list" "(" expression (arbno "," expression) ")" )
+     list-exp)
+
+    ;; new productions for oop
+    (class-decl
+     ("class" identifier
+      "extends" identifier
+      (arbno "implements" identifier)
+      (arbno "field" type identifier)
+      (arbno method-decl)
+      )
+     a-class-decl)
+
+    (method-decl
+     ("method" type identifier
+      "("  (separated-list identifier  ":" type ",") ")" ; method formals
+      expression
+      )
+     a-method-decl)
+
+    (expression
+     ("new" identifier "(" (separated-list expression ",") ")")
+     new-object-exp)
+
+    ;; this is special-cased to prevent it from mutation
+    (expression
+     ("self")
+     self-exp)
+
+    (expression
+     ("send" expression identifier
+      "("  (separated-list expression ",") ")")
+     method-call-exp)
+
+    (expression
+     ("super" identifier    "("  (separated-list expression ",") ")")
+     super-call-exp)
+
+    ;; new productions for typed-oo
+
+    (class-decl
+     ("interface" identifier (arbno abstract-method-decl))
+     an-interface-decl)
+
+
+    (abstract-method-decl
+     ("method" type identifier
+      "("  (separated-list identifier ":" type ",") ")" )
+     an-abstract-method-decl)
+
+    (expression
+     ("cast" expression identifier)
+     cast-exp)
+
+    (expression
+     ("instanceof" expression identifier)
+     instanceof-exp)
+
+    (type ("int") int-type)
+    (type ("bool") bool-type)
+    (type ("void") void-type)
+    (type
+     ("(" (separated-list type "*") "->" type ")")
+     proc-type)
+    (type
+     ("listof" type)
+     list-type)
+
+    (type (identifier) class-type) ;; new for typed oo
+
+    ))
+
+  ;;;;;;;;;;;;;;;; sllgen boilerplate ;;;;;;;;;;;;;;;;
+
+(sllgen:make-define-datatypes the-lexical-spec the-grammar)
+
+(define show-the-datatypes
+  (lambda () (sllgen:list-define-datatypes the-lexical-spec the-grammar)))
+
+(define scan&parse
+  (sllgen:make-string-parser the-lexical-spec the-grammar))
+
+(define just-scan
+  (sllgen:make-string-scanner the-lexical-spec the-grammar))
+
+;;;;;;;;;;;;;;;; syntactic operations on types ;;;;;;;;;;;;;;;;
+
+(run "class c1 extends object
+       method int initialize () 1
+       method int m1 () 11
+       staticmethod int m2 () 21
+      class c2 extends c1
+       method void m1 () 12
+       staticmethod int m2 () 22
+      let f = proc(x : c1) send x m1()
+          g = proc (x : c1) send x m2()
+          o = new c2()
+      in list((f o), (g o))")
+
+;;(run-all)
