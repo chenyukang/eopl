5dd2071ff2424082856c1f965166b9f4b6dc616c
AssemblyCoffee <1686560661@qq.com>
2014-05-26 10:41:14 +0800

    Update 2.27.scm

--- ch2/2.27.scm
+++ ch2/2.27.scm
@@ -1,4 +1,4 @@
-(load "../libs/init.scm")
+#lang eopl
 
 
 ;; I won't give a picture for the AST, but to construct such too AST, the code is
@@ -6,8 +6,8 @@
 
 (define id?
   (lambda (symbol)
-    (not (and (symbol? symbol)
-              (eqv? symbol 'lambda)))))
+    (and (symbol? symbol)
+         (not (eqv? symbol 'lambda)))))
 
 (define-datatype lc-expr lc-expr?
   (var-expr
@@ -22,11 +22,11 @@
 
 (define expA
   (app-expr
-   (lambda-expr
-	       (var-expr 'a)
+      (lambda-expr
+	      'a
 	       (var-expr 'b))
    (var-expr 'c)))
-
+;#(struct:app-expr #(struct:lambda-expr a #(struct:var-expr b)) #(struct:var-expr c))
 (define expB
   (lambda-expr 'x
 	      (lambda-expr 'y
@@ -36,3 +36,12 @@
 						   (app-expr (var-expr 'x)
 							    (var-expr 'y)))
 				       (var-expr 'x))))))
+;;#(struct:lambda-expr
+;;  x
+;;  #(struct:lambda-expr
+;;    y
+;;    #(struct:lambda-expr
+;;      x
+;;      #(struct:app-expr
+;;        #(struct:lambda-expr x #(struct:app-expr #(struct:var-expr x) #(struct:var-expr y)))
+;;        #(struct:var-expr x)))))
