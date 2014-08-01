af71dbc5071a9b7efefd85d1ee820cdf45cda52c
Yukang <moorekang@gmail.com>
2014-08-02 00:39:22 +0800

    continue 36, constructor interp with effects

---
+++ ch6/36.scm
@@ -0,0 +1,16 @@
+;; Add a begin expression(exercise4.4) to CPS-IN.
+;; You should not need to add anything to CPS-OUT
+
+;; TODO
+(load-relative "../libs/init.scm")
+(load-relative "./base/test.scm")
+(load-relative "./base/cps-effects.scm")
+(load-relative "./base/data-structures-effects.scm")
+(load-relative "./base/store-effects.scm")
+(load-relative "./base/cps-in-lang.scm")
+(load-relative "./base/cps-out-lang.scm")
+(load-relative "./base/interp-effects.scm")
+(load-relative "./base/cps-effects-cases.scm")
+
+
+(run-all)
